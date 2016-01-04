f7c62671999ec29549021f797789b5cda79d3c25
grebett <gregory.betton@gmail.com>
2016-01-04 23:25:06 +0100

    Working with structs – chained list

--- main.go
+++ main.go
@@ -5,18 +5,32 @@ import (
 	"fmt"
 )
 
-type Person struct {
-	firstName string
-	lastName  string
-	age       int
+type List struct {
+	value string
+	next  *List
 }
 
 func main() {
-	p := &Person{"Grégory", "Betton", 30}
-	q := new(Person)
+	var node *List
+	var begin *List
 
-	q.firstName = "Marc"
-	q.lastName = "Twain"
-	q.age = 186
-	fmt.Printf("%v and %v", p, q)
+	begin = new(List)
+	begin.value = "this is the start node"
+	node = addNode(begin, "and this is a second node")
+	node = addNode(node, "and this is a third node")
+
+	node = begin
+	for node != nil {
+		fmt.Println(node.value)
+		node = node.next
+	}
+}
+
+func addNode(node *List, value string) *List {
+	newNode := new(List)
+
+	newNode.value = value
+	node.next = newNode
+
+	return newNode
 }
