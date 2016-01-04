7c4d688f2768c493a14f2056ef55b6c241089c2e
grebett <gregory.betton@gmail.com>
2016-01-04 23:06:53 +0100

    Working with maps

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
-// Working with arrays – vertices by reference
+// Working with maps
 package main
 
 import (
 	"fmt"
 )
 
-type vertex [3]int
-
 func main() {
-	a := vertex{25, -12, 4}
-	invert(&a)
-	fmt.Println(a)
-}
+	var m = map[string]int{"one": 1, "two": 2}
+	n := make(map[string]int)
 
-func invert(v *vertex) {
-	for i := range v {
-		v[i] *= -1
+	n["three"] = 3
+	n["for"] = 4
+
+	fmt.Println(m)
+	fmt.Println(n)
+
+	if _, ok := n["five"]; ok {
+		fmt.Println("`five` key exists")
+	} else {
+		fmt.Println("`five` key does not exist")
 	}
 }
