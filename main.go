b1c338ff68c12197f118de2b4261ddda1c7a26e9
grebett <gregory.betton@gmail.com>
2016-01-04 15:20:02 +0100

    Working with slices II

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Working with slices
+// Working with slices II
 package main
 
 import (
@@ -6,14 +6,11 @@ import (
 )
 
 func main() {
-	a := [...]int{0, 1, 2, 3, 4, 5}
-	d := &a
+	str := "Hello world!"
+	slice := []rune(str)
 
-	fmt.Printf("len of a: %d – cap of a: %d\n", len(a), cap(a))
-	fmt.Printf("len of d: %d – cap of d: %d\n", len(d), cap(d))
-
-	d[0] = 1
-	for _, value := range a {
-		fmt.Println(value)
+	for i := range slice {
+		slice[i] += 1
 	}
+	fmt.Printf(string(slice))
 }
