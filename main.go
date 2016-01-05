ee5d6a5e4a15baee054cdf38edfcf3e8ec95c488
grebett <gregory.betton@gmail.com>
2016-01-05 18:42:55 +0100

    Error handling – custom error

--- main.go
+++ main.go
@@ -2,11 +2,31 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"time"
 )
 
+// creating a custom struct
+type CustomError struct {
+	when    time.Time
+	message string
+}
+
+// using this struct as an error - i.e adding an Error method to the struct
+func (e *CustomError) Error() string {
+	return fmt.Sprintf("Error (%v): %s", e.when, e.message)
+}
+
+func Test(i int) (int, error) {
+	if i < 0 {
+		// calling the error method immediatly using a struct pointer
+		return 0, &CustomError{time.Now(), "the i number should not be negative"}
+	} else {
+		return i, nil
+	}
+}
+
 func main() {
-	err := errors.New("this is a very basic string error")
-	fmt.Printf("Error: %v\n", err)
+	fmt.Println(Test(-1))
+	fmt.Println(Test(1))
 }
